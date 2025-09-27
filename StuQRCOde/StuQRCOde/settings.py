cd3b20e458b77d564192aad3c37de81b8a9b0a09
christopher Banyani <christopherbanyanipinto@gmail.com>
2025-09-27 04:04:20 +0200

    Fix database configuration to default to SQLite when DATABASE_URL not set

--- StuQRCOde/StuQRCOde/settings.py
+++ StuQRCOde/StuQRCOde/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'StuQRCOde.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=600,
         conn_health_checks=True,
     )
